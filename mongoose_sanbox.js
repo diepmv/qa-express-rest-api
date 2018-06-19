76e55daabd6a2d581358105152209210eb6bdf5c
Mai Diep <diepmv@vccloud.vn>
2018-06-20 06:28:37 +0700

    add mongoose test file

---
+++ mongoose_sanbox.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/sanbox');
+
+var db = mongoose.connection;
+
+db.on('error', function(err) {
+	console.error('Connection error', err)
+});
+
+db.once("open", function() {
+	console.log('db connection successfully');
+	// all db communication goes here
+
+	var Schema = mongoose.Schema;
+	var AnimalSchema = new Schema({
+		type: String,
+		size: String,
+		color: String,
+		mass: Number,
+		name: String
+	});
+
+
+	var Animal = mongoose.model("Animal", AnimalSchema);
+
+	var elephant = new Animal({
+		type: "elephant",
+		size: "big",
+		color: "gray",
+		mass: 6000,
+		name: "Lawrance"
+	});
+
+
+	elephant.save(function(err){
+		if (err) console.error("Save Failed.", err);
+		else console.log("Saved");
+		db.close(function() {
+			console.log("db connection closed");
+		});
+	});
+
+	
+});
